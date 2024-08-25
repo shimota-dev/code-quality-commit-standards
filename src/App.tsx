9ab6730fb0b5ccaf56722adf0e469e39ac7ad8b8
Wataru Shimota <w.shimota@gmail.com>
2024-08-25 09:40:00 +0900

    chore: lint on commitmsg

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  let name;
 
   return (
     <>
@@ -19,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
